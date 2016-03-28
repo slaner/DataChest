445daf46e74282257b18fe6c2b4b0809c0590473
SlaneR <devslaner@gmail.com>
2016-03-28 23:24:25 +0900

    Added License Information.

--- DataChest/ChestAPI/Types/Algorithms.cs
+++ DataChest/ChestAPI/Types/Algorithms.cs
@@ -1,4 +1,29 @@
-﻿/// <summary>
+﻿/*
+  Copyright (c) 2016 HYE WON, HWANG
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/// <summary>
 /// 암,복호화에 사용될 알고리즘을 열거합니다.<br />
 /// SEED 알고리즘은 성능으로 인해 제외되었습니다.
 /// </summary>
