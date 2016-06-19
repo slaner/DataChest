8d348e9d9802d6bd6180f712d516d658dacb1a93
Hye won, Hwang <black_scv@naver.com>
2016-06-20 00:28:55 +0900

    Simplify source code.

--- DataChest/DataChest/DataChest.cs
+++ DataChest/DataChest/DataChest.cs
@@ -22,7 +22,20 @@ namespace DataChest {
         /// Default buffer size.
         /// </summary>
         public const int DefaultBufferSize = 4096;
-
+        /// <summary>
+        /// IV가 지정되지 않은 경우에 기본 값으로 사용되는 값입니다.<br />
+        /// Default used value when IV is not specified.
+        /// </summary>
+        /// <remarks>
+        /// 이 바이트 배열은 2부터 시작하여 총 32개의 소수로 만들어졌습니다.<br />
+        /// This byte array begins 2 and created with 32 prime numbers.
+        /// </remarks>
+        public static readonly byte[] DefaultIV = {
+            0x02, 0x03, 0x05, 0x07, 0x0B, 0x0D, 0x11, 0x13,
+            0x17, 0x1D, 0x1F, 0x25, 0x29, 0x2B, 0x2F, 0x35,
+            0x3B, 0x3D, 0x43, 0x47, 0x49, 0x4F, 0x53, 0x59,
+            0x61, 0x65, 0x67, 0x6B, 0x6D, 0x71, 0x7F, 0x83,
+        };
 
         Option                  m_option    = null;
         SymmetricAlgorithm      m_alg       = null;
@@ -41,6 +54,218 @@ namespace DataChest {
             m_option = option;
         }
 
+
+        /// <summary>
+        /// 주어진 작업에 대한 테스트를 수행합니다.<br />
+        /// Perform a test for given task.
+        /// </summary>
+        /// <param name="s">
+        /// 데이터를 저장하고 있는 <see cref="Stream"/> 개체입니다.<br />
+        /// A instance of <see cref="Stream"/> that contains data.
+        /// </param>
+        TaskResult TestProcess(Stream s) {
+            TaskResult r = 0;
+            byte[] result = null;
+            HeaderBase hdr = null;
+            uint hash = 0;
+            
+            if (m_option.Decrypt) {
+                hdr = HeaderManager.FromStream(s, out r);
+                if (r != TaskResult.Success) {
+                    s.Dispose();
+                    return r;
+                }
+                
+                if (!m_option.DisableVerification) {
+                    long lnPrevPos = s.Position;
+                    hash = HashAPI.ComputeHashUInt32(s);
+                    
+                    if (hdr.EChecksum != hash) {
+                        s.Dispose();
+                        return TaskResult.IncorrectEncryptedDataChecksum;
+                    }
+                    
+                    s.Seek(lnPrevPos, SeekOrigin.Begin);
+                }
+            }
+
+            r = m_option.Decrypt ? Decrypt(s, m_alg, out result) : Encrypt(s, m_alg, out result);
+            if (r != TaskResult.Success) {
+                s.Dispose();
+                return r;
+            }
+
+            if (m_option.DisableVerification || !m_option.Decrypt) {
+                s.Dispose();
+                return r;
+            }
+            
+            hash = HashAPI.ComputeHashUInt32(result);
+            if (hdr.RChecksum != hash) {
+                s.Dispose();
+                return TaskResult.IncorrectRawDataChecksum;
+            }
+
+            s.Dispose();
+            return TaskResult.Success;
+        }
+        /// <summary>
+        /// 암호화 작업을 수행합니다.<br />
+        /// Perform encryption process.
+        /// </summary>
+        /// <param name="sin">
+        /// 암호화될 데이터가 저장된 <see cref="Stream"/> 개체입니다.<br />
+        /// A instance of <see cref="Stream"/> class that contains data to be encrypted.
+        /// </param>
+        /// <param name="sout">
+        /// 암호화된 데이터가 저장될 <see cref="Stream"/> 개체입니다.<br />
+        /// A instance of <see cref="Stream"/> class to encrypted data be stored.
+        /// </param>
+        /// <param name="output">
+        /// 출력 파일의 이름입니다.<br />
+        /// A name of output file.
+        /// </param>
+        TaskResult EncryptProcess(Stream sin, Stream sout, string output) {
+            byte[] result;
+            TaskResult r = Encrypt(sin, m_alg, out result);
+            if (r != TaskResult.Success) {
+                sin.Dispose();
+                sout.Dispose();
+                return r;
+            }
+
+            ushort v = m_option.HeaderVersion;
+            if (v == 0) v = HeaderManager.NewerVersion;
+
+            HeaderBase hdr = HeaderManager.Create(v, out r);
+            if (r != TaskResult.Success) {
+                sin.Dispose();
+                sout.Dispose();
+                return r;
+            }
+            r = hdr.AssignBasicInformationEncrypt(sin, result);
+
+            byte[] header;
+            r = hdr.ToArray(out header);
+            if (r != TaskResult.Success) {
+                sin.Dispose();
+                sout.Dispose();
+                return r;
+            }
+            
+            try {
+                using (BinaryWriter bw = new BinaryWriter(sout)) {
+                    bw.Write(header);
+                    
+                    int loop_count = result.Length / m_option.BufferSize.Value;
+                    int remain = result.Length % m_option.BufferSize.Value;
+                    for (int i = 0; i < loop_count; i++)
+                        bw.Write(result, i * m_option.BufferSize.Value, m_option.BufferSize.Value);
+
+                    if (remain > 0)
+                        bw.Write(result, loop_count * m_option.BufferSize.Value, remain);
+                }
+            }
+            catch {
+                sin.Dispose();
+                FileHelper.DeleteFileIgnoreErrors(output);
+                return TaskResult.StreamWriteError;
+            }
+
+            if (m_option.Cleanup) {
+                // TODO: Multiple file support?
+                try { File.Delete(m_option.In[0]); } catch {
+                    sin.Dispose();
+                    return TaskResult.CleanupFailedSucceed;
+                }
+            }
+            
+            return TaskResult.Success;
+        }
+        /// <summary>
+        /// 복호화 작업을 수행합니다.<br />
+        /// Perform encryption process.
+        /// </summary>
+        /// <param name="sin">
+        /// 복호화될 데이터가 저장된 <see cref="Stream"/> 개체입니다.<br />
+        /// A instance of <see cref="Stream"/> class that contains data to be decrypted.
+        /// </param>
+        /// <param name="sout">
+        /// 복호화된 데이터가 저장될 <see cref="Stream"/> 개체입니다.<br />
+        /// A instance of <see cref="Stream"/> class to decrypted data be stored.
+        /// </param>
+        /// <param name="output">
+        /// 출력 파일의 이름입니다.<br />
+        /// A name of output file.
+        /// </param>
+        TaskResult DecryptProcess(Stream sin, Stream sout, string output) {
+            byte[] result;
+            TaskResult r;
+
+            HeaderBase hdr = HeaderManager.FromStream(sin, out r);
+            if (r != TaskResult.Success) {
+                sin.Dispose();
+                sout.Dispose();
+                FileHelper.DeleteFileIgnoreErrors(output);
+                return r;
+            }
+
+            if (!m_option.DisableVerification) {
+                long lnPrevPos = sin.Position;
+                uint prevHash = HashAPI.ComputeHashUInt32(sin);
+
+                if (hdr.EChecksum != prevHash) {
+                    sin.Dispose();
+                    sout.Dispose();
+                    FileHelper.DeleteFileIgnoreErrors(output);
+                    return TaskResult.IncorrectEncryptedDataChecksum;
+                }
+
+                sin.Seek(lnPrevPos, SeekOrigin.Begin);
+            }
+
+            r = Decrypt(sin, m_alg, out result);
+            if (r != TaskResult.Success) {
+                sin.Dispose();
+                sout.Dispose();
+                return r;
+            }
+
+            if (!m_option.DisableVerification) {
+                if (HashAPI.ComputeHashUInt32(result) != hdr.RChecksum) {
+                    sin.Dispose();
+                    sout.Dispose();
+                    FileHelper.DeleteFileIgnoreErrors(output);
+                    return TaskResult.IncorrectEncryptedDataChecksum;
+                }
+            }
+
+            try {
+                using (BinaryWriter bw = new BinaryWriter(sout)) {
+                    int loop_count = result.Length / m_option.BufferSize.Value;
+                    int remain = result.Length % m_option.BufferSize.Value;
+                    for (int i = 0; i < loop_count; i++)
+                        bw.Write(result, i * m_option.BufferSize.Value, m_option.BufferSize.Value);
+
+                    if (remain > 0)
+                        bw.Write(result, loop_count * m_option.BufferSize.Value, remain);
+                }
+            } catch {
+                sin.Dispose();
+                FileHelper.DeleteFileIgnoreErrors(output);
+                return TaskResult.StreamWriteError;
+            }
+
+            if (m_option.Cleanup) {
+                try { File.Delete(m_option.In[0]); } catch {
+                    sin.Dispose();
+                    return TaskResult.CleanupFailedSucceed;
+                }
+            }
+
+            return TaskResult.Success;
+        }
+
         public TaskResult Process() {
             if (m_option.In.Count == 0) return TaskResult.NoInputFile;
             if (m_option.ShowHeaderInfo) {
@@ -54,21 +279,86 @@ namespace DataChest {
             m_alg = AlgorithmManager.CreateAlgorithm((int)m_option.Algorithm);
             if (m_alg == null) return TaskResult.InvalidAlgorithm;
 
+            TaskResult r;
+            r = SetupAlgorithm();
+            if (r != TaskResult.Success) return r;
 
-            return TaskResult.Success;
+            FileStream fs;
+            r = FileHelper.OpenFileStream(m_option.In[0], out fs);
+            if (r != TaskResult.Success) return r;
+
+            if (m_option.RunTest) return TestProcess(fs);
+
+            string output;
+            r = FileHelper.BuildOutput(m_option, out output);
+            
+            if (r != TaskResult.Success) {
+                fs.Dispose();
+                return r;
+            }
+
+            FileStream ofs;
+            try { ofs = new FileStream(output, m_option.Overwrite ? FileMode.Create : FileMode.CreateNew); }
+            catch (IOException) {
+                fs.Dispose();
+                return TaskResult.FileAlreadyExists;
+            }
+            catch {
+                fs.Dispose();
+                return TaskResult.IOError;
+            }
+
+            r = m_option.Decrypt ? DecryptProcess(fs, ofs, output) : EncryptProcess(fs, ofs, output);
+            if (r != TaskResult.Success) FileHelper.DeleteFileIgnoreErrors(output);
+            return r;
         }
 
         public void Dispose() {
 
         }
-        
+
+        /// <summary>
+        /// 작업에 사용되는 암호 및 IV를 설정합니다.<br />
+        /// Set a password and IV used by cryptographic function.
+        /// </summary>
+        TaskResult SetupAlgorithm() {
+            TaskResult r;
+            string pw, iv = null;
+            if (string.IsNullOrEmpty(m_option.Password)) pw = IDHelper.MachineUserName();
+            else pw = m_option.Password;
+
+            Stream spw, siv;
+
+            r = GetDataStream(pw, out spw);
+            if (r != TaskResult.Success) return r;
+
+            byte[] temp = new byte[m_alg.Key.Length];
+            Buffer.BlockCopy(HashAPI.ComputeHash(spw), 0, temp, 0, temp.Length);
+            m_alg.Key = temp;
+            spw.Dispose();
+
+            if (string.IsNullOrEmpty(m_option.IV)) {
+                temp = new byte[m_alg.IV.Length];
+                Buffer.BlockCopy(DefaultIV, 0, temp, 0, temp.Length);
+                m_alg.IV = temp;
+            } else {
+                r = GetDataStream(iv, out siv);
+                if (r != TaskResult.Success) return r;
+                temp = new byte[m_alg.IV.Length];
+                Buffer.BlockCopy(HashAPI.ComputeHash(siv), 0, temp, 0, temp.Length);
+                m_alg.IV = temp;
+                spw.Dispose();
+            }
+
+            return TaskResult.Success;
+        }
         /// <summary>
         /// 옵션을 검사합니다.<br />
         /// Check an option.
         /// </summary>
         bool CheckOptions() {
             List<bool> chkopt = new List<bool>();
-            chkopt.Add(m_option.Cleanup || m_option.Overwrite);
+            chkopt.Add(m_option.Cleanup && m_option.Overwrite);
             chkopt.Add(m_option.RunTest && (!string.IsNullOrEmpty(m_option.Out)));
             foreach (bool statement in chkopt) {
                 if (statement) return false;
@@ -93,14 +383,28 @@ namespace DataChest {
             if (source.StartsWith(CipherFromFile)) {
                 FileStream fs;
                 TaskResult r = FileHelper.OpenFileStream(source.Substring(CipherFromFile.Length), out fs);
-                s = fs;
+                if (r == TaskResult.FileNotFound) {
+                    byte[] b = Common.SystemEncoding.GetBytes(source.Substring(CipherFromFile.Length));
+                    s = new MemoryStream(b);
+                } else if (r == TaskResult.Success) s = fs;
                 return r;
             } else if (source.StartsWith(CipherPlainText)) {
-                FileStream fs;
-                TaskResult r = FileHelper.OpenFileStream(source.Substring(CipherFromFile.Length), out fs);
-                s = fs;
-                return r;
-            } else return TaskResult.InvalidPhrasePrefix;
+                try {
+                    byte[] b = Common.SystemEncoding.GetBytes(source.Substring(CipherPlainText.Length));
+                    s = new MemoryStream(b);
+                    return TaskResult.Success;
+                } catch (EncoderFallbackException) {
+                    return TaskResult.EncodingError;
+                }
+            } else {
+                try {
+                    byte[] b = Common.SystemEncoding.GetBytes(source);
+                    s = new MemoryStream(b);
+                    return TaskResult.Success;
+                } catch (EncoderFallbackException) {
+                    return TaskResult.EncodingError;
+                }
+            }
         }
         /// <summary>
         /// 헤더의 정보를 출력합니다.<br />
@@ -109,7 +413,11 @@ namespace DataChest {
         TaskResult ShowHeaderInfo() {
             TaskResult r;
             HeaderBase hdr = HeaderManager.FromFile(m_option.In[0], out r);
-            if (r == TaskResult.Success) Console.WriteLine(hdr.TraverseProperties().ToString());
+            if (r == TaskResult.Success)
+                Console.WriteLine(hdr.TraverseProperties().ToString());
+            else {
+                Console.WriteLine("ERROR: INVALID_HEADER_OR_NOT_VALID_FILE ({0})", r);
+            }
             return r;
         }
 
@@ -132,12 +440,11 @@ namespace DataChest {
         TaskResult Encrypt(Stream s, SymmetricAlgorithm alg, out byte[] result) {
             result = null;
             MemoryStream ms = new MemoryStream();
-            int size = m_option.BufferSize.Value;
 
             ICryptoTransform ct = alg.CreateEncryptor();
             using (CryptoStream cs = new CryptoStream(ms, ct, CryptoStreamMode.Write)) {
                 byte[] temp;
-                try { temp = new byte[size]; }
+                try { temp = new byte[m_option.BufferSize.Value]; }
                 catch {
                     ct.Dispose();
                     ms.Dispose();
@@ -145,7 +452,7 @@ namespace DataChest {
                 }
                 
                 int nRead;
-                try { nRead = s.Read(temp, 0, size); }
+                try { nRead = s.Read(temp, 0, m_option.BufferSize.Value); }
                 catch {
                     ct.Dispose();
                     return TaskResult.StreamReadError;
@@ -157,7 +464,7 @@ namespace DataChest {
                         return TaskResult.StreamWriteError;
                     }
 
-                    try { nRead = s.Read(temp, 0, size); } catch {
+                    try { nRead = s.Read(temp, 0, m_option.BufferSize.Value); } catch {
                         ct.Dispose();
                         return TaskResult.StreamReadError;
                     }
@@ -203,7 +510,7 @@ namespace DataChest {
                 catch {
                     SafeDispose(cs);
                     ct.Dispose();
-                    return TaskResult.StreamReadError;
+                    return TaskResult.InvalidPasswordOrDataCorrupted;
                 }
 
                 while (nRead > 0) {
@@ -213,7 +520,7 @@ namespace DataChest {
                     } catch {
                         SafeDispose(cs);
                         ct.Dispose();
-                        return TaskResult.StreamReadError;
+                        return TaskResult.InvalidPasswordOrDataCorrupted;
                     }
                 }
             }
