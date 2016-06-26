7a4010f938b7685bb712a15c551efaa152d5467c
Hye won, Hwang <black_scv@naver.com>
2016-06-26 17:56:58 +0900

    UnitHelper class has been added for performance logging.

--- DataChest/DataChest/Helper/UnitHelper.cs
+++ DataChest/DataChest/Helper/UnitHelper.cs
@@ -17,56 +17,80 @@
   along with DataChest.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using System;
+using System.Diagnostics;
+
 namespace DataChest {
+    /// <summary>
+    /// 크기 및 진행 시간을 이용하여 처리 속도를 계산하는 함수를 노출합니다.<br />
+    /// Exposes function that computing process speed using size and progress time.
+    /// </summary>
     static class UnitHelper {
-        static string[] dataUnit = { "B", "KB", "MB", "GB", "TB", "PB" };
-        static string[] spdUnit = { "B/s", "Kb/s", "Mb/s", "Gb/s", "Tb/s", "Pb/s" };
-
-        public const long PB = 1125899906842624L;
-        public const long TB = 1099511627776L;
-        public const int GB = 1073741824;
-        public const int MB = 1048576;
-        public const int KB = 1024;
-
-        static byte GetDataDelimiter(double size) {
-            if (size >= PB)
-                return 5;
-            else if (size >= TB)
-                return 4;
-            else if (size >= GB)
-                return 3;
-            else if (size >= MB)
-                return 2;
-            else if (size >= KB)
-                return 1;
-            else return 0;
+        enum DataSize : byte {
+            B,
+            KiB,
+            MiB,
+            GiB,
+            TiB,
+            PiB,
         }
-        public static string ToReadableSize(double size, bool speed) {
-            byte unit = GetDataDelimiter(size);
-            double measured;
-            switch (unit) {
-                case 5:
-                    measured = size / PB;
+        
+        public const long PiB = 1125899906842624L;
+        public const long TiB = 1099511627776L;
+        public const int GiB = 1073741824;
+        public const int MiB = 1048576;
+        public const int KiB = 1024;
+        static readonly double TickFrequency = 10000000.0 / Stopwatch.Frequency;
+
+        static DataSize GetDataSize(double size) {
+            if (size >= PiB) return DataSize.PiB;
+            if (size >= TiB) return DataSize.TiB;
+            if (size >= GiB) return DataSize.GiB;
+            if (size >= MiB) return DataSize.MiB;
+            if (size >= KiB) return DataSize.KiB;
+            return DataSize.B;
+        }
+        static double GetElapsedTimeFromTick(long tick) {
+            return (tick * TickFrequency) / 10000000.0;
+        }
+
+        /// <summary>
+        /// 처리 속도를 계산합니다.<br />
+        /// Compute a process speed.
+        /// </summary>
+        /// <param name="size">
+        /// 처리한 데이터 크기입니다.<br />
+        /// Size of proceed data.
+        /// </param>
+        /// <param name="elapsed">
+        /// 처리 시간입니다.<br />
+        /// Time of proceed.
+        /// </param>
+        public static string ComputeSpeed(long size, TimeSpan elapsed) {
+            double measured = size / GetElapsedTimeFromTick(elapsed.Ticks);
+            DataSize ds = GetDataSize(measured);
+
+            switch (ds) {
+                case DataSize.PiB:
+                    measured /= PiB;
                     break;
-                case 4:
-                    measured = size / TB;
+                case DataSize.TiB:
+                    measured /= TiB;
                     break;
-                case 3:
-                    measured = size / GB;
+                case DataSize.GiB:
+                    measured /= GiB;
                     break;
-                case 2:
-                    measured = size / MB;
+                case DataSize.MiB:
+                    measured /= MiB;
                     break;
-                case 1:
-                    measured = size / KB;
+                case DataSize.KiB:
+                    measured /= KiB;
                     break;
                 default:
-                    measured = size;
                     break;
             }
 
-            if (speed) return string.Format("{0:F} {1}", measured, spdUnit[unit]);
-            return string.Format("{0:F} {1}", measured, dataUnit[unit]);
+            return string.Format("{0:F} {1}/s", measured, ds.ToString());
         }
     }
 }
