d5f0d5fc792cca14e543b582e08573375bad5a88
Hye won, Hwang <black_scv@naver.com>
2016-06-26 17:57:17 +0900

    PerformanceLogger class has been added.

--- DataChest/DataChest/Performance/PerformanceLogger.cs
+++ DataChest/DataChest/Performance/PerformanceLogger.cs
@@ -20,39 +20,210 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
+using System.IO;
 using System.Text;
 
 namespace DataChest {
-    static class PerformanceLogger {
-        static Stopwatch m_sw = new Stopwatch();
-        static ulong m;
-
-        static string LastCryptographyPerformance = "<결과 없음>";
-        static string LastFileIOPerformance = "<결과 없음>";
-        static ulong LastFileSize = 0;
-        static TimeSpan LastEntireTaskTime = TimeSpan.MinValue;
-
-
-
+    /// <summary>
+    /// 성능 기록기입니다.<br />
+    /// A performance logger.
+    /// </summary>
+    sealed class PerformanceLogger {
         /// <summary>
-        /// 성능 기록기를 시작합니다.
+        /// 성능 기록에 사용되는 체크포인트입니다.<br />
+        /// Checkpoint used by performance logging.
         /// </summary>
-        public static void Begin() {
-            m_sw.Restart();
+        public sealed class Checkpoint {
+            string g_name;
+            TimeSpan g_time;
+            TimeSpan g_elapsed;
+
+            /// <summary>
+            /// 이름과 시간을 이용해 체크포인트를 만듭니다.<br />
+            /// Create checkpoint using name and time.
+            /// </summary>
+            /// <param name="name">
+            /// 체크포인트의 이름입니다.<br />
+            /// Name of checkpoint.
+            /// </param>
+            /// <param name="time">
+            /// 체크포인트 도달 시간입니다.<br />
+            /// Reached time of checkpoint.
+            /// </param>
+            public Checkpoint(string name, TimeSpan time) {
+                g_name = name;
+                g_time = time;
+            }
+
+            /// <summary>
+            /// 체크포인트 이름을 가져옵니다.<br />
+            /// Get a name of checkpoint.
+            /// </summary>
+            public string Name {
+                get { return g_name; }
+            }
+            /// <summary>
+            /// 체크포인트 도달 시간을 가져옵니다.<br />
+            /// Get a reached time of checkpoint.
+            /// </summary>
+            public TimeSpan Time {
+                get { return g_time; }
+            }
+            /// <summary>
+            /// 체크포인트가 열린 후부터 진행된 시간을 가져옵니다.<br />
+            /// Get a elapsed time of checkpoint after opened.
+            /// </summary>
+            public TimeSpan ElapsedTime {
+                get { return g_elapsed; }
+            }
+
+            /// <summary>
+            /// 체크포인트를 마칩니다.<br />
+            /// Finish a checkpoint.
+            /// </summary>
+            /// <param name="endTime">
+            /// 체크포인트 종료 시간입니다.<br />
+            /// Time of checkpoint ended.
+            /// </param>
+            public void Finish(TimeSpan endTime) {
+                g_elapsed = endTime - g_time;
+            }
+
+            /// <summary>
+            /// 체크포인트를 문자열로 나타냅니다.<br />
+            /// Represents the checkpoint as a string.
+            /// </summary>
+            public override string ToString() {
+                return string.Format("{0} - {1}", Name, Time);
+            }
+        }
+
+        readonly Stopwatch m_sw;
+        readonly bool g_enabled;
+        readonly StringBuilder m_buffer;
+        int m_checkpointLevels = 0;
+        
+        public PerformanceLogger(DataChest dc) {
+            g_enabled = dc.Option.Verbose;
+            if (g_enabled) {
+                m_sw = new Stopwatch();
+                m_buffer = new StringBuilder();
+            }
+        }
+        
+        /// <summary>
+        /// 성능 기록을 시작합니다.<br />
+        /// Start a performance logging.
+        /// </summary>
+        /// <param name="s">
+        /// 성능 기록에 참조할 <see cref="Stream"/> 입니다.<br />
+        /// <see cref="Stream"/> to reference performance logging.
+        /// </param>
+        public void Start() {
+            if (!Enabled) return;
+            
+            m_sw.Start();
+            m_buffer.AppendLine(SR.GetString("DC_PerformanceLogging_Start"));
         }
 
         /// <summary>
-        /// 성능 기록기를 종료합니다.
+        /// 체크포인트를 엽니다.<br />
+        /// Open a checkpoint.
         /// </summary>
-        public static void End() {
+        /// <param name="name">
+        /// 체크포인트의 이름입니다.<br />
+        /// Name of checkpoint.
+        /// </param>
+        public Checkpoint OpenCheckpoint(string name) {
+            if (!Enabled) return null;
+            if (!m_sw.IsRunning) return null;
+
+            m_buffer.Append(' ', m_checkpointLevels * 2);
+            m_buffer.AppendLine(string.Format(SR.GetString("DC_PerformanceLogging_Checkpoint_Created"), m_sw.Elapsed, name));
+            m_checkpointLevels++;
+            return new Checkpoint(name, m_sw.Elapsed);
+        }
+        /// <summary>
+        /// 체크포인트를 닫습니다.<br />
+        /// Close a checkpoint.
+        /// </summary>
+        /// <param name="checkpoint">
+        /// <see cref="OpenCheckpoint(string)"/> 메서드에 의해 열린 체크포인트입니다.<br />
+        /// A checkpoint that is opened by <see cref="OpenCheckpoint(string)"/> method.
+        /// </param>
+        /// <param name="size">
+        /// 처리된 데이터의 크기입니다.<br />
+        /// Size of data handled.
+        /// </param>
+        public void CloseCheckpoint(Checkpoint checkpoint, long size) {
+            if (!Enabled) return;
+            if (!m_sw.IsRunning) return;
+            if (checkpoint == null) return;
+            if (m_checkpointLevels <= 0) return;
+
+            checkpoint.Finish(m_sw.Elapsed);
             m_sw.Stop();
+            string s;
+            if (size == 0) s = string.Format("<{0}>", SR.GetString("DC_PerformanceLogging_None"));
+            else s = UnitHelper.ComputeSpeed(size, checkpoint.ElapsedTime);
+            m_buffer.Append(' ', (m_checkpointLevels - 1) * 2);
+            m_buffer.AppendLine(
+                string.Format(
+                    SR.GetString("DC_PerformanceLogging_Checkpoint_Finished"),
+                    m_sw.Elapsed,
+                    checkpoint.Name,
+                    SR.GetString("DC_PerformanceLogging_Speed"),
+                    s,
+                    SR.GetString("DC_PerformanceLogging_ElapsedTime"),
+                    checkpoint.ElapsedTime));
+            m_checkpointLevels--;
+            m_sw.Start();
+
         }
 
+        /// <summary>
+        /// 성능 기록을 중단합니다.<br />
+        /// Abort a performance logging.
+        /// </summary>
+        /// <param name="r">
+        /// 작업 결과입니다.<br />
+        /// Task result.
+        /// </param>
+        public TaskResult Abort(TaskResult r) {
+            if (!Enabled) return r;
 
+            m_sw.Stop();
+            m_buffer.AppendLine(
+                string.Format(
+                    SR.GetString("DC_PerformanceLogging_Aborted"),
+                    SR.GetString("DC_PerformanceLogging_Error"),
+                    r.ToString()));
+            return r;
+        }
+        /// <summary>
+        /// 성능 기록을 마칩니다.
+        /// End a performance logging.
+        /// </summary>
+        public void End() {
+            if (!Enabled) return;
+            m_sw.Stop();
 
-        static void CalculateSpeed() {
+            m_buffer.AppendLine(SR.GetString("DC_PerformanceLogging_Ended"));
+        }
+        /// <summary>
+        /// 성능 기록을 반환합니다.<br />
+        /// Returns a performance log.
+        /// </summary>
+        public override string ToString() {
+            return m_buffer.ToString();
+        }
 
+        /// <summary>
+        /// 성능 기록기 사용 가능 여부를 가져옵니다.<br />
+        /// Get an availability of performance logger.
+        /// </summary>
+        public bool Enabled {
+            get { return g_enabled; }
         }
     }
 }
